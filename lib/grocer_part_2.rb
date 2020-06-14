60a2fef1e17c27e1ccde9ad01d8902200add195a
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-06-14 16:24:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
        new_cart << {:item => "#{cart_item[:item]} W/COUPON",
        :price => coupon_item[:cost] / coupon_item[:num],
        :clearance => cart_item[:clearance],
-       
+       :count => 
  end
  end
  end

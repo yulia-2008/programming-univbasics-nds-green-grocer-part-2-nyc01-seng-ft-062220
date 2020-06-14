99fb7de7fa1e1c6fd4abd9d1510ead7f50f957c9
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-06-14 17:36:54 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,8 +38,8 @@ end
 
 def checkout(cart, coupons)
   total=0
-  #apply_coupons(coupons, consolidate_cart(cart))
-  cart_after_discounts = apply_clearance(apply_coupon(coupon, consolidate_cart(cart)))
+  #apply_coupons(consolidate_cart(cart),coupons, )
+  cart_after_discounts = apply_clearance(apply_coupons(coupons, consolidate_cart(cart)))
   cart_after_discounts.each do |item|
    total+= item[:price]*iten[:count]
  end   
